27d5d73ea3743db3a934da43d66f72c9dc299046
wangari <accienjenga@gmail.com>
2023-09-18 08:15:21 -0700

    first push

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+
+create a class - Base
+"""
+
+
+class Base:
+    """private class attribute"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """instance/ attributes of base class"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects =+ 1
+            self.id = Base.__nb_objects
+
